a7b2f3e8ec0b11a4ff18eeea807e4038124e849c
Rafael de Jesús Ramos Chávez <158338467+RafiuX40@users.noreply.github.com>
2024-05-22 13:55:32 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Game.hpp"
+using namespace std;
+using namespace sf;
+
+
+
+int main() {
+    RenderWindow window(VideoMode(450, 400), "SFML works!");
+    window.setFramerateLimit(60);
+
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+
+                Game game;
+                game.Gameloop();
+                return 0;
+        }
+
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
